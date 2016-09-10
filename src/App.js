b1aa977cfd440efaafd2a835b0c27bc4524d0fff
Andy <andybriggs84@gmail.com>
2016-09-10 16:52:23 +0100

    minor style tweak

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <link rel="stylesheet" type="text/css" href="/static/style.css" />
-      <h1>My New App.</h1>
+      <h1>My New App!</h1>
       <img src={img} />
     </div>
   );
