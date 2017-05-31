d5d38b84c0eb25f9af13ef50cd74a6fc86d4058b
Andy <andybriggs84@gmail.com>
2017-05-31 22:27:34 +0100

    removed redundant files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes.js';
+import App from './app.js';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
