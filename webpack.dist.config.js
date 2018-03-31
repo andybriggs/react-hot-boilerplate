06bd1b370fe2583035a4cae69398725a062cead6
Andy <andybriggs84@gmail.com>
2018-03-31 11:22:27 +0100

    added env var to create minified react bundle for prod enviroment

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -10,6 +10,9 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin("style.css", { allChunks: true }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: false,
       mangle: false
